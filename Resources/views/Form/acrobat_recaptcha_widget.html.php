58c0a1db2acfd1ab1b01b9fb19b0c6fb95454579
Jeroen Thora <jeroenthora@gmail.com>
2014-10-18 16:23:05 +0200

    Fixed php syntax errors

--- Resources/views/Form/acrobat_recaptcha_widget.html.php
+++ Resources/views/Form/acrobat_recaptcha_widget.html.php
@@ -1,5 +1,5 @@
 <?php if ($recaptcha_enabled): ?>
-    <?php if !($recaptcha_ajax): ?>
+    <?php if (!$recaptcha_ajax): ?>
         <?php if (isset($attr['options'])): ?>
             <script type="text/javascript">
             var RecaptchaOptions = <?php echo json_encode($attr['options']) ?>;
@@ -11,7 +11,7 @@
             <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
             <input type="hidden" name="recaptcha_response_field" value="manual_challenge"/>
         </noscript>
-    <?php else ?>
+    <?php else: ?>
         <div id="recaptcha_div"></div>
 
         <script type="text/javascript" src="<?php echo $url_api ?>"></script>
